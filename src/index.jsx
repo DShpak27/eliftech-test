67f9ee389044f2a10228226363ece23809a4f605
Denys Shpak <mr.shpak@gmail.com>
2024-06-11 18:54:45 +0300

    vercel deploy settings

--- src/index.jsx
+++ src/index.jsx
@@ -12,40 +12,37 @@ import WelcomePage from './pages/ShopPage/WelcomePage/index.js';
 import CurrentShopPage from './pages/ShopPage/CurrentShopPage/index.js';
 import './index.css';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Layout />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: 'shop',
-          element: <ShopPage />,
-          children: [
-            {
-              index: true,
-              element: <WelcomePage />,
-            },
-            {
-              path: ':shopId',
-              element: <CurrentShopPage />,
-            },
-          ],
-        },
-        {
-          path: 'shopping-cart',
-          element: <CartPage />,
-        },
-        {
-          path: 'history',
-          element: <HistoryPage />,
-        },
-      ],
-    },
-  ],
-  { basename: '/eliftech-test' }
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'shop',
+        element: <ShopPage />,
+        children: [
+          {
+            index: true,
+            element: <WelcomePage />,
+          },
+          {
+            path: ':shopId',
+            element: <CurrentShopPage />,
+          },
+        ],
+      },
+      {
+        path: 'shopping-cart',
+        element: <CartPage />,
+      },
+      {
+        path: 'history',
+        element: <HistoryPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
