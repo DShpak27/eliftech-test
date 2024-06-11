3280be1c68a03ae09f2936a61ff5bb8cdd43668e
Denys Shpak <mr.shpak@gmail.com>
2024-06-11 14:11:55 +0300

    perf: changed server back-end

--- src/redux/order/api.js
+++ src/redux/order/api.js
@@ -1,28 +1,29 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const orderApi = createApi({
-    reducerPath: "order",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "https://eliftech-back-on7b.onrender.com/",
+  reducerPath: 'order',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://eliftech-back-beta.vercel.app/',
+    // baseUrl: "https://eliftech-back-on7b.onrender.com/",
+  }),
+  tagTypes: ['Orders'],
+  endpoints: builder => ({
+    getOrders: builder.query({
+      query: email => ({
+        url: `orders?email=${email}`,
+        method: 'GET',
+      }),
+      providesTags: ['Orders'],
     }),
-    tagTypes: ["Orders"],
-    endpoints: (builder) => ({
-        getOrders: builder.query({
-            query: (email) => ({
-                url: `orders?email=${email}`,
-                method: "GET",
-            }),
-            providesTags: ["Orders"],
-        }),
-        placeOrder: builder.mutation({
-            query: (body) => ({
-                url: "orders",
-                method: "POST",
-                body,
-            }),
-            providesTags: ["Orders"],
-        }),
+    placeOrder: builder.mutation({
+      query: body => ({
+        url: 'orders',
+        method: 'POST',
+        body,
+      }),
+      providesTags: ['Orders'],
     }),
+  }),
 });
 
 export default orderApi;
--- src/redux/shop/api.js
+++ src/redux/shop/api.js
@@ -1,26 +1,27 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const shopApi = createApi({
-    reducerPath: "shop",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "https://eliftech-back-on7b.onrender.com/",
+  reducerPath: 'shop',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://eliftech-back-beta.vercel.app/',
+    // baseUrl: "https://eliftech-back-on7b.onrender.com/",
+  }),
+  endpoints: builder => ({
+    getShops: builder.query({
+      query: () => ({
+        url: 'pharms',
+        method: 'GET',
+      }),
+      providesTags: ['Shops'],
     }),
-    endpoints: (builder) => ({
-        getShops: builder.query({
-            query: () => ({
-                url: "pharms",
-                method: "GET",
-            }),
-            providesTags: ["Shops"],
-        }),
-        getShopPrice: builder.query({
-            query: (id) => ({
-                url: `pharms/${id}/prices`,
-                method: "GET",
-            }),
-            providesTags: ["Price"],
-        }),
+    getShopPrice: builder.query({
+      query: id => ({
+        url: `pharms/${id}/prices`,
+        method: 'GET',
+      }),
+      providesTags: ['Price'],
     }),
+  }),
 });
 
 export default shopApi;
