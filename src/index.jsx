2faddc293658104dee2887d411b4c3fdf277335d
Denys Shpak <mr.shpak@gmail.com>
2024-06-11 14:28:55 +0300

    perf: changed base name

--- src/index.jsx
+++ src/index.jsx
@@ -1,56 +1,53 @@
-import ReactDOM from "react-dom/client";
-import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store.js";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Layout from "./components/Layout";
-import ShopPage from "./pages/ShopPage";
-import CartPage from "./pages/CartPage";
-import HistoryPage from "./pages/HistoryPage";
-import ErrorPage from "./pages/ErrorPage";
-import WelcomePage from "./pages/ShopPage/WelcomePage";
-import CurrentShopPage from "./pages/ShopPage/CurrentShopPage";
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store.js';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import ShopPage from './pages/ShopPage';
+import CartPage from './pages/CartPage';
+import HistoryPage from './pages/HistoryPage';
+import ErrorPage from './pages/ErrorPage';
+import WelcomePage from './pages/ShopPage/WelcomePage';
+import CurrentShopPage from './pages/ShopPage/CurrentShopPage';
+import './index.css';
 
-const router = createBrowserRouter(
-    [
-        {
-            path: "/",
-            element: <Layout />,
-            errorElement: <ErrorPage />,
-            children: [
-                {
-                    path: "shop",
-                    element: <ShopPage />,
-                    children: [
-                        {
-                            index: true,
-                            element: <WelcomePage />,
-                        },
-                        {
-                            path: ":shopId",
-                            element: <CurrentShopPage />,
-                        },
-                    ],
-                },
-                {
-                    path: "shopping-cart",
-                    element: <CartPage />,
-                },
-                {
-                    path: "history",
-                    element: <HistoryPage />,
-                },
-            ],
-        },
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'shop',
+        element: <ShopPage />,
+        children: [
+          {
+            index: true,
+            element: <WelcomePage />,
+          },
+          {
+            path: ':shopId',
+            element: <CurrentShopPage />,
+          },
+        ],
+      },
+      {
+        path: 'shopping-cart',
+        element: <CartPage />,
+      },
+      {
+        path: 'history',
+        element: <HistoryPage />,
+      },
     ],
-    { basename: "/eliftech-test" }
-);
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <RouterProvider router={router} />
-        </PersistGate>
-    </Provider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <RouterProvider router={router} />
+    </PersistGate>
+  </Provider>
 );
---
+++ src/index_reserved.jsx
@@ -0,0 +1,56 @@
+import ReactDOM from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store.js';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout/index.js';
+import ShopPage from './pages/ShopPage/index.js';
+import CartPage from './pages/CartPage/index.js';
+import HistoryPage from './pages/HistoryPage/index.js';
+import ErrorPage from './pages/ErrorPage/index.js';
+import WelcomePage from './pages/ShopPage/WelcomePage/index.js';
+import CurrentShopPage from './pages/ShopPage/CurrentShopPage/index.js';
+import './index.css';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: 'shop',
+          element: <ShopPage />,
+          children: [
+            {
+              index: true,
+              element: <WelcomePage />,
+            },
+            {
+              path: ':shopId',
+              element: <CurrentShopPage />,
+            },
+          ],
+        },
+        {
+          path: 'shopping-cart',
+          element: <CartPage />,
+        },
+        {
+          path: 'history',
+          element: <HistoryPage />,
+        },
+      ],
+    },
+  ],
+  { basename: '/eliftech-test' }
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <RouterProvider router={router} />
+    </PersistGate>
+  </Provider>
+);
